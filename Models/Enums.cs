7b49b23f736199997299abb0cd9bc68be9d81203
Daniel <97480083+Dasshuii@users.noreply.github.com>
2025-08-06 12:43:27 -0600

    Library Management System Draft. Initial Commit

---
+++ Models/Enums.cs
@@ -0,0 +1,19 @@
+namespace TCSA.OOP.LibraryManagementSystem.Models;
+
+internal class Enums
+{
+    internal enum MenuAction
+    {
+        ViewItem,
+        AddItem,
+        DeleteItem,
+    }
+
+    internal enum ItemType
+    {
+        Book,
+        Magazine,
+        Newspaper
+    }
+
+}
---
+++ UserInterface.cs
@@ -0,0 +1,93 @@
+using Spectre.Console;
+using TCSA.OOP.LibraryManagementSystem.Controllers;
+using static TCSA.OOP.LibraryManagementSystem.Models.Enums;
+
+namespace TCSA.OOP.LibraryManagementSystem;
+
+internal class UserInterface
+{
+    private readonly BooksController _booksController = new();
+    private readonly MagazineController _magazineController = new();
+    private readonly NewspaperController _newspaperController = new();
+    internal void MainMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+
+            var actionChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<MenuAction>()
+                .Title("What do you want to do next?")
+                .AddChoices(Enum.GetValues<MenuAction>())
+            );
+
+            var itemTypeChoice = AnsiConsole.Prompt(
+                new SelectionPrompt<ItemType>()
+                .Title("Select the type of item:")
+                .AddChoices(Enum.GetValues<ItemType>())
+            );
+
+            switch (actionChoice)
+            {
+                case MenuAction.ViewItem:
+                    ViewItems(itemTypeChoice);
+                    break;
+                case MenuAction.AddItem:
+                    AddItem(itemTypeChoice);
+                    break;
+                case MenuAction.DeleteItem:
+                    DeleteItem(itemTypeChoice);
+                    break;
+            }
+        }
+    }
+
+    private void ViewItems(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            case ItemType.Book:
+                _booksController.ViewItems();
+                break;
+            case ItemType.Magazine:
+                _magazineController.ViewItems();
+                break;
+            case ItemType.Newspaper:
+                _newspaperController.ViewItems();
+                break;
+        }
+    }
+
+    private void AddItem(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            case ItemType.Book:
+                _booksController.AddItem();
+                break;
+            case ItemType.Magazine:
+                _magazineController.AddItem();
+                break;
+            case ItemType.Newspaper:
+                _newspaperController.AddItem();
+                break;
+        }
+    }
+
+    private void DeleteItem(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            case ItemType.Book:
+                _booksController.DeleteItem();
+                break;
+            case ItemType.Magazine:
+                _magazineController.DeleteItem();
+                break;
+            case ItemType.Newspaper:
+                _newspaperController.DeleteItem();
+                break;
+        }
+    }
+}
+
